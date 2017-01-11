a46edc7ff5acef65f8854345b81baa9fbac99b69
Cphil31 <cecile_philippe@live.fr>
2017-01-11 10:28:30 +0100

    step 1 fait

---
+++ index1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+
+	<form action="user1.php" method="GET">
+
+		<label for="">Nom</label>
+		<input type="text" name="Nom">
+		<label for="">Prénom </label>
+		<input type="text" name ="Prénom">
+		<input type="submit" name="submit" value="Valider">
+
+	</form>
+
+	<?php 
+ 
+
+
+
+	?>
+	
+</body>
+</html>
