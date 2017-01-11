c72c5a505a259b97a8e59d6d21942cf7c4702a89
Cphil31 <cecile_philippe@live.fr>
2017-01-11 10:33:20 +0100

    step 2 fait

---
+++ index2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<form action="user2.php" method="POST">
+
+		<label for="">Nom</label>
+		<input type="text" name="Nom">
+		<label for="">Prénom </label>
+		<input type="text" name ="Prénom">
+		<input type="submit" name="submit" value="Valider">
+
+	</form>
+
+	
+</body>
+</html>
